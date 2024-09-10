1d98df67be4832608574921e0b974b679788b6ee
Syed Taha <syetaha@gmail.com>
2024-09-10 17:15:11 +0500

    Add @vercel/analytics package for tracking website analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 // Import Poppins font from Google Fonts
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${poppins.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
